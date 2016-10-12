30c48e4bebfb0346a8ccd40b06fe3d00f9da9d5a
Allex Rodrigues <allexxrodriguess@gmail.com>
2016-10-11 22:14:31 -0300

    Add files via upload

---
+++ SerialComm/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SerialComm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Serial = new System.IO.Ports.SerialPort(this.components);
+            this.btAbrirCon = new System.Windows.Forms.Button();
+            this.lbConState = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbNivel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btAbrirCon
+            // 
+            this.btAbrirCon.Location = new System.Drawing.Point(12, 12);
+            this.btAbrirCon.Name = "btAbrirCon";
+            this.btAbrirCon.Size = new System.Drawing.Size(75, 23);
+            this.btAbrirCon.TabIndex = 0;
+            this.btAbrirCon.Text = "Abrir Porta";
+            this.btAbrirCon.UseVisualStyleBackColor = true;
+            this.btAbrirCon.Click += new System.EventHandler(this.btAbrirCon_Click);
+            // 
+            // lbConState
+            // 
+            this.lbConState.AutoSize = true;
+            this.lbConState.Location = new System.Drawing.Point(12, 38);
+            this.lbConState.Name = "lbConState";
+            this.lbConState.Size = new System.Drawing.Size(94, 13);
+            this.lbConState.TabIndex = 1;
+            this.lbConState.Text = "Conexão fechada.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbNivel);
+            this.groupBox1.Location = new System.Drawing.Point(112, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(71, 298);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nível";
+            // 
+            // lbNivel
+            // 
+            this.lbNivel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbNivel.BackColor = System.Drawing.Color.Red;
+            this.lbNivel.Location = new System.Drawing.Point(10, 25);
+            this.lbNivel.Name = "lbNivel";
+            this.lbNivel.Size = new System.Drawing.Size(52, 256);
+            this.lbNivel.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(197, 318);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbConState);
+            this.Controls.Add(this.btAbrirCon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort Serial;
+        private System.Windows.Forms.Button btAbrirCon;
+        private System.Windows.Forms.Label lbConState;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbNivel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SerialComm/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Modbus.Data;
+using Modbus.Device;
+using Modbus.Utility;
+using System.IO.Ports;
+
+namespace SerialComm
+{
+    public partial class Form1 : Form
+    {
+        IModbusSerialMaster modbus;
+        Label lbParteVerde;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Serial.PortName = "COM3";
+            Serial.BaudRate = 19200;
+            Serial.DataBits = 8;
+            Serial.Parity = Parity.None;
+            Serial.StopBits = StopBits.One;
+
+            lbParteVerde = new Label();
+            lbParteVerde.BackColor = Color.Green;
+            lbParteVerde.AutoSize = false;
+            lbParteVerde.Width = lbNivel.Width;
+            lbParteVerde.Height = 0;
+            lbParteVerde.CreateControl();
+            lbNivel.Controls.Add(lbParteVerde);
+            lbParteVerde.BringToFront();
+
+        }
+
+        private void btAbrirCon_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Serial.Open();
+                modbus = ModbusSerialMaster.CreateRtu(Serial);
+                lbConState.Text = "Conexão aberta.";
+                timer1.Enabled = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro Serial :" + ex.Message);
+                this.Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ushort inicio = 0;
+            ushort qnt = 1;
+            byte slave = 1;
+
+            ushort M1 = 0;
+
+            try
+            {
+                ushort[] registers = modbus.ReadHoldingRegisters(slave, inicio, qnt);
+                M1 = registers[0];
+
+                double val = (M1 / 4)+1;
+
+                if (val <= 15)
+                {
+                    lbParteVerde.Height = 0;
+                }
+                else if (val >= 1023)
+                {
+
+                }
+                else
+                {
+                    lbParteVerde.Height = (int)Math.Floor(val);
+                }
+                
+                lbParteVerde.Location = new Point(0, lbNivel.Height - lbParteVerde.Height);
+                lbParteVerde.BringToFront();
+                
+                label1.Text = "Lido: " + M1.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                timer1.Enabled = false;
+                lbConState.Text = "Conexão fechada.";
+                MessageBox.Show(ex.Message);
+            }  
+        }
+    }
+}
